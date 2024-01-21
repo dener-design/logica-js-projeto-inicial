9b08cce55f3d28e2c74ec3d23b17d0afd825efdd
Dener Patrick <dener@outlook.com>
2024-01-20 21:21:05 -0300

    Aplicando Template Strings

--- app.js
+++ app.js
@@ -13,59 +13,59 @@
 
 // 1º Desafio
 
-// //Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
-// alert('Boas vindas ao nosso site!');
+//Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+alert('Boas vindas ao nosso site!');
 
-// //Declare uma variável chamada nome e atribua a ela o valor "Lua".
-// let nome = 'Lua';
+//Declare uma variável chamada nome e atribua a ela o valor "Lua".
+let nome = 'Lua';
 
-// //Crie uma variável chamada idade e atribua a ela o valor 25.
-// let idade = 25;
+//Crie uma variável chamada idade e atribua a ela o valor 25.
+let idade = 25;
 
-// //Defina uma variável numeroDeVendas e atribua a ela o valor 50.
-// let numeroDeVendas = 50;
+//Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+let numeroDeVendas = 50;
 
-// //Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
-// let saldoDisponivel = 1000;
+//Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+let saldoDisponivel = 1000;
 
-// //Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
-// let mensagemDeErro = 'Erro! Preencha todos os campos';
+//Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
+let mensagemDeErro = 'Erro! Preencha todos os campos';
 
-// // Novas entradas dos usuários para armazená Nome e idade
+// Novas entradas dos usuários para armazená Nome e idade
 
-// //Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
-// let nomeDoUsuario = prompt('Digite seu nome:');
+//Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
+let nomeDoUsuario = prompt('Digite seu nome:');
 
-// //Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
-// let idadeDoUsuario = Number(prompt('Digite sua idade:'));
+//Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+let idadeDoUsuario = Number(prompt('Digite sua idade:'));
 
 
-// //Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
-// if (idadeDoUsuario >= 18){
-//     alert( nomeDoUsuario + ', Você pode tirar a habilitação!');
-// } else{
-//     alert( nomeDoUsuario + ', você não pode tirar a habilitação!');
-// }
+//Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+if (idadeDoUsuario >= 18){
+    alert(`${nomeDoUsuario}, Você pode tirar a habilitação!`);
+} else{
+    alert(`${nomeDoUsuario}, você não pode tirar a habilitação!`);
+}
 
-// //Verrifica se o usuário digitou em todos os campos
-// if(!nomeDoUsuario || !idade){
-//     console.log('O usuário não digitou todos os campos pedidos no prompt')
-//     alert(mensagemDeErro);
-// }
+//Verrifica se o usuário digitou em todos os campos
+if(!nomeDoUsuario || !idade){
+    console.log('O usuário não digitou todos os campos pedidos no prompt')
+    alert(mensagemDeErro);
+}
 
 
 // 2º Desafio
 
-alert('Boa vindas ao jogo do número secreto');
+// alert('Boa vindas ao jogo do número secreto');
 
-let numeroSecreto = 5;
-console.log(numeroSecreto);
+// let numeroSecreto = 5;
+// console.log(numeroSecreto);
 
-let valorChute = prompt('Escolha um número entre 1 e 10');
+// let valorChute = prompt('Escolha um número entre 1 e 10');
 
-// se chute for igual ao número secreto 
-if (valorChute == numeroSecreto){
-    alert('Isso aí! Você acertou o número secreto (5)');
-} else {
-    alert('Você errou =(');
-}
+// // se chute for igual ao número secreto 
+// if (valorChute == numeroSecreto){
+//     alert(`Isso aí! Você acertou o número secreto ${numeroSecreto}`);
+// } else {
+//     alert('Você errou =(');
+// }
